48d08485b23b36e808828c2d0d450d79ddb72af8
Luis Alvarez <luisalvarez@Luiss-Air.home>
2019-11-09 16:16:09 -0500

    changed the look of the page with some filler text. next up i have to form the footer then add actual content

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('this is working')
 
 let mission = document.getElementById('mission')
+let sticky = document.getElementsByClass('stick')
 
 mission.addEventListener('click', function() {
   alert('you clicked this!')
